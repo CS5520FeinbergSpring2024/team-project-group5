9d53546bfd96786f2f325e026403d2cde3e362c5
honoyomu <50334568+honoyomu@users.noreply.github.com>
2024-03-23 00:47:40 -0400

    homepage setup

--- app/src/main/java/edu/northeastern/pawpalsgroup5/MainActivity.java
+++ app/src/main/java/edu/northeastern/pawpalsgroup5/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.northeastern.pawpalsgroup5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        // Load the default PlaceholderFragment when the activity is first created
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, PlaceholderFragment.newInstance(1)).commit();
+        }
     }
+
+    private final NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+//            switch (item.getItemId()) {
+//                case R.id.nav_home:
+//                    selectedFragment = PlaceholderFragment.newInstance(1);
+//                    break;
+//                case R.id.nav_following:
+//                    selectedFragment = PlaceholderFragment.newInstance(2);
+//                    break;
+//                case R.id.nav_post:
+//                    selectedFragment = PlaceholderFragment.newInstance(3);
+//                    break;
+//                case R.id.nav_me:
+//                    selectedFragment = PlaceholderFragment.newInstance(4);
+//                    break;
+//                case R.id.nav_settings:
+//                    selectedFragment = PlaceholderFragment.newInstance(5);
+//                    break;
+//            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+
+            return true; // Indicate that the item selection has been handled
+        }
+    };
 }
