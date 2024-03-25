8c7779e1bd7423d797a5539967c9e240945548ed
jli <jinyan_li@yahoo.com>
2024-03-25 16:05:20 -0700

    add Message model

---
+++ app/src/main/java/edu/northeastern/pawpalsgroup5/models/Message.java
@@ -0,0 +1,50 @@
+package edu.northeastern.pawpalsgroup5.models;
+
+public class Message {
+    private String senderId;
+    private String receiverId;
+    private String text;
+    private long timestamp;
+
+    public Message() {
+    }
+
+    public Message(String senderId, String receiverId, String text, long timestamp) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+    
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
