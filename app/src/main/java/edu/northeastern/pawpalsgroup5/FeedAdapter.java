b68e01ecf480ce860937e9b4b4f1961664646349
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2024-03-29 14:27:17 -0700

    feed ui/backend implemented

---
+++ app/src/main/java/edu/northeastern/pawpalsgroup5/FeedAdapter.java
@@ -0,0 +1,83 @@
+package edu.northeastern.pawpalsgroup5;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+import edu.northeastern.pawpalsgroup5.models.Post;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder> {
+    private List<Post> posts;
+
+    public FeedAdapter(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public FeedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_feed, parent, false);
+        return new FeedViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeedViewHolder holder, int position) {
+        Post post = posts.get(position);
+
+        holder.descriptionTextView.setText(post.getDescription());
+        holder.usernameTextView.setText(post.getUserId());
+        holder.numLikeTextView.setText(String.format("%d likes", post.getLikes()));
+        // handle image loading
+//        Glide.with(context).load(post.getPicture()).into(holder.profileImageView);
+
+        holder.likeImageView.setOnClickListener(v -> {
+            // Increment the like count in the model
+            post.setLikes(post.getLikes() + 1);
+            holder.numLikeTextView.setText(String.format("%d likes", post.getLikes()));
+
+            // Update the like count in Firebase
+            DatabaseReference postRef = FirebaseDatabase.getInstance().getReference("posts").child(post.getPostId()); // Ensure you have a postId or similar unique identifier
+            postRef.child("likes").setValue(post.getLikes()).addOnCompleteListener(task -> {
+                if (!task.isSuccessful()) {
+                    post.setLikes(post.getLikes() - 1);
+                    holder.numLikeTextView.setText(String.format("%d likes", post.getLikes()));
+                }
+            });
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    static class FeedViewHolder extends RecyclerView.ViewHolder {
+        ImageView profileImageView;
+        ImageView likeImageView;
+        ImageView chatImageView;
+        TextView numLikeTextView;
+        TextView usernameTextView;
+        TextView descriptionTextView;
+
+        public FeedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profileImageView = itemView.findViewById(R.id.profileImageView);
+            likeImageView = itemView.findViewById(R.id.likeImageView);
+            chatImageView = itemView.findViewById(R.id.chatImageView);
+            descriptionTextView = itemView.findViewById(R.id.descriptionTextView);
+            numLikeTextView = itemView.findViewById(R.id.numLikeTextView);
+            usernameTextView = itemView.findViewById(R.id.usernameTextView);
+        }
+    }
+}
